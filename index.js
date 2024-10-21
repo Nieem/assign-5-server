8ddb07986780a91dce9af06a9c77bb0dfbab43ed
Nieem <nieemcse019@gmail.com>
2024-10-21 19:49:21 +0600

    regi data insert done

---
+++ index.js
@@ -0,0 +1,55 @@
+const express=require("express");
+const cors=require("cors");
+require("dotenv").config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app=express();
+const port=process.env.port || 5000;
+
+app.use(cors());
+app.use(express.json());
+console.log(process.env.DB_USER)
+// ${process.env.DB_USER}
+// ${process.env.DB_SECRET}
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_SECRET}@test1.rgboj.mongodb.net/?retryWrites=true&w=majority&appName=test1`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const userCollection=client.db("bootCampdbFull").collection("users");
+
+    app.post("/users",async(req,res)=>{
+        const users=req.body;
+        console.log(users);
+        const result=await userCollection.insertOne(users);
+        res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+   // await client.close();
+  }
+}
+run().catch(console.dir);
+app.get("/",(req,res)=>{
+    res.send("Bootcamp react node crud is running");
+}
+);
+
+app.listen(port, ()=>{
+    console.log(`Bootcamp react node crud is running on ${port}`);
+})
